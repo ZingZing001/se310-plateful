f8ee9799f953feddf72b5612268eae77bdcde7d6
Kimberley Z <kimbe@wifi-172-23-53-111.uoa-wifi.auckland.ac.nz>
2025-08-17 16:11:13 +1200

    home page image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import backgroundImage from "../assets/PlatefulBackgroundHome copy.png";
+import "../styles/home.css";
 
 export default function Home() {
   return (
-    <div className="search-wrapper">
-      <img
-        src={backgroundImage}
-        alt="Background"
-        className="full-width-image"
-      />
+    <div className="showcase">
+      <section className="search-wrapper">
+        <img
+          src={backgroundImage}
+          alt="Background"
+          className="full-width-image"
+        />
+      </section>
     </div>
   );
 }
