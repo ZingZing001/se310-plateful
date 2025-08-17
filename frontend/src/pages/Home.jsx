dc4bed831ca876c94d05223a0bc887777db9767f
Kimberley Z <kimbe@wifi-172-23-53-111.uoa-wifi.auckland.ac.nz>
2025-08-17 17:47:45 +1200

    Make image fit whole width of screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,29 @@
 import backgroundImage from "../assets/PlatefulBackgroundHome copy.png";
 import "../styles/home.css";
+import "../styles/global.css";
 
 export default function Home() {
   return (
-    <div className="showcase">
+    <div>
       <section className="search-wrapper">
         <img
           src={backgroundImage}
           alt="Background"
-          className="full-width-image"
+          className="background-image"
         />
         <div className="search-bar">
           <input type="text" placeholder="Search..." />
           <button>Go</button>
+        </div>
+      </section>
+
+      <section className="cuisine-section">
+        <h2>Explore Cuisines</h2>
+        <div className="cuisine-list">
+          <div className="cuisine-item">Italian</div>
+          <div className="cuisine-item">Chinese</div>
+          <div className="cuisine-item">Mexican</div>
+          <div className="cuisine-item">Indian</div>
         </div>
       </section>
     </div>
