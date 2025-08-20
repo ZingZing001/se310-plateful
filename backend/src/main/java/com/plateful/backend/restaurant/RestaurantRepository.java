eac9554d29965d74ac24076a83916588825b5392
oorjagandhi <oorjagandhi@gmail.com>
2025-08-20 19:18:35 +1200

    feat: add search method to RestaurantRepository

--- backend/src/main/java/com/plateful/backend/restaurant/RestaurantRepository.java
+++ backend/src/main/java/com/plateful/backend/restaurant/RestaurantRepository.java
@@ -1,5 +1,9 @@
 package com.plateful.backend.restaurant;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
-public interface RestaurantRepository extends MongoRepository<Restaurant, String> {}
+public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+    List<Restaurant> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrCuisineContainingIgnoreCase(
+        String name, String description, String cuisine);
+}
